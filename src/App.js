4c09c23b8ccec42c62668ff1ea70aea9d1517303
hodjabedir <hancimen@gmail.com>
2023-02-16 12:11:37 +0300

    display page changed

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
 import './App.css';
 
 import DeliveryCreate from './Views/DeliveryCreate';
+import DataDisplay from './Views/DataDisplay';
+
 const data = [
   { id: 1,statu :'Acil', depoTuru:'Kuru Gıda', depoAdi: 'Merkez Kızılay', depoAddress: 'Merkez/K.Maraş'},
+
+];
+
+const columnsDepo = [
+  { field: 'id', headerName: 'Depo No', width: 90 },
+
+{ field: 'statu', headerName: 'Statu', width: 80 },
+{ field: 'depoTuru', headerName: 'Depo Türü', width: 120 },
+{ field: 'depoAdi', headerName: 'Depo Adı', width: 130 },
+{ field: 'depoAddress', headerName: 'Depo Adres', width: 140 },
+
+];
+
+const columnsSevkiyat = [
+{ field: 'id', headerName: 'Sevkiyat No', width: 90 },
+{ field: 'trackID', headerName: 'Plaka No', width: 90 },
+{ field: 'statu', headerName: 'Statu', width: 80 },
+{ field: 'depoTuru', headerName: 'Çıkış Yeri', width: 120 },
+{ field: 'depoAdi', headerName: 'Varış Yeri', width: 130 },
+{ field: 'depoAddress', headerName: 'Yük Türü', width: 140 },
 
 ];
 function App() {
@@ -10,6 +32,9 @@ function App() {
     <div>
     
       <DeliveryCreate ></DeliveryCreate>
+      <DataDisplay data={data} columns ={columnsDepo}></DataDisplay>
+      <DataDisplay data={data} columns ={columnsSevkiyat}></DataDisplay>
+
     </div>
   );
 }
---
+++ src/Views/DataDisplay.js
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import { DataGrid } from '@mui/x-data-grid';
+
+
+export default function DataTable(props) {
+  return (
+    <div style={{ height: 400, width: '100%' }}>
+      <DataGrid
+        rows={props.data}
+        columns={props.columns}
+        pageSize={5}
+        rowsPerPageOptions={[5]}
+        checkboxSelection
+      />
+    </div>
+  );
+}
